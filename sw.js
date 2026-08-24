06a9611ef1792e95dafedbb54cf30f89bf258572
bkngt <bkngt.np@gmail.com>
2026-08-24 15:09:34 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'murali-quiz-v75';
+const CACHE_NAME = 'murali-quiz-v79';
 const URLS_TO_CACHE = [
   './',
   './index.html',
@@ -28,14 +28,20 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
+// NETWORK FIRST STRATEGY (सधैं नयाँ भर्सन तान्ने नियम)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; 
-        }
-        return fetch(event.request); 
+    fetch(event.request)
+      .then(networkResponse => {
+        // इन्टरनेट छ भने नयाँ भर्सन ल्याउने र सेभ गर्ने
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
+        // इन्टरनेट छैन भने मात्र पुरानो सेभ भएको भर्सन देखाउने
+        return caches.match(event.request);
       })
   );
 });
