409c87e5c7e1d07688d7143eddabffc9d7f073a0
bkngt <bkngt.np@gmail.com>
2026-09-16 09:09:52 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'murali-quiz-v90-1-0';
+const CACHE_NAME = 'murali-quiz-v90-2-0';
 
 const URLS_TO_CACHE = [
   './',
@@ -8,7 +8,6 @@ const URLS_TO_CACHE = [
 ];
 
 self.addEventListener('install', event => {
-  // नयाँ Service Worker लाई तुरुन्तै waiting state मा नराखी activate गराउने
   self.skipWaiting();
 
   event.waitUntil(
@@ -30,12 +29,11 @@ self.addEventListener('activate', event => {
     })
   );
 
-  // नयाँ Service Worker ले तुरुन्तै सबै open pages control गरोस्
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  // POST/API request cache नगर्ने
+  // Google Apps Script/API requests should always go directly to network.
   if (
     event.request.method !== 'GET' ||
     event.request.url.includes('script.google.com')
@@ -46,14 +44,12 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
       .then(networkResponse => {
-        // सफल network response लाई नयाँ cache मा राख्ने
         return caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, networkResponse.clone());
           return networkResponse;
         });
       })
       .catch(() => {
-        // Internet नभए cache बाट चलाउने
         return caches.match(event.request);
       })
   );
