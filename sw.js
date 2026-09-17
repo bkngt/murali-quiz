86fdd2b8545656ed803d7935647ed4c0e8c8bb0d
bkngt <bkngt.np@gmail.com>
2026-09-17 20:51:40 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,56 +1,52 @@
-const CACHE_NAME = 'murali-quiz-v90-2-8';
+const CACHE_NAME = 'murali-quiz-v91-0';
 
-const APP_SHELL = [
+const URLS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
+      .then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    ).then(() => self.clients.claim())
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
+
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  // Google Apps Script / API requests should always go to network.
-  if (event.request.url.includes('script.google.com')) {
+  if (
+    event.request.method !== 'GET' ||
+    event.request.url.includes('script.google.com')
+  ) {
     return;
   }
 
   event.respondWith(
     fetch(event.request)
-      .then(response => {
-        // Cache only successful same-origin responses.
-        if (
-          response &&
-          response.ok &&
-          new URL(event.request.url).origin === self.location.origin
-        ) {
-          const responseClone = response.clone();
-
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, responseClone);
-          });
-        }
-
-        return response;
+      .then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
       })
       .catch(() => {
         return caches.match(event.request);
